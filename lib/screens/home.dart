73bfc515ec514b5a574f67bc2dac6b53118ccd8b
nikunjjain333 <67869256+nikunjjain333@users.noreply.github.com>
2021-09-27 13:52:18 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.arrow_back),
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut().whenComplete(
+                    () => Navigator.popAndPushNamed(context, 'login'));
+              },
+              icon: Icon(Icons.logout))
+        ],
         title: Text('My Home Page'),
         backgroundColor: Colors.blueGrey,
       ),
